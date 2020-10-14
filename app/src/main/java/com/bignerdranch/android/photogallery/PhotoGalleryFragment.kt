2fb65ab25b49b32ef4cf849271fa215cbf6791ae
Daniel Molina <dannyfacio14@gmail.com>
2020-10-13 21:29:23 -0700

    Added Flickr depdency, interface, and converter

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,15 +1,49 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        //use retrofit object to create an instance of the API interface
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+
+        //get an object representing an executable web request
+        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrHomePageRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received: ${response.body()}")
+            }
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +58,6 @@ class PhotoGalleryFragment : Fragment() {
 
         return view
     }
-
 
     companion object {
         fun newInstance() = PhotoGalleryFragment()
