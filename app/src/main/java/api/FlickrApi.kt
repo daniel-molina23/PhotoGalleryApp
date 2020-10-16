6a429a83238dc2323db62e963547ecded2363175
Daniel Molina <dannyfacio14@gmail.com>
2020-10-15 19:46:52 -0700

    Added background thread to execute API commands instead of UI

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -1,7 +1,9 @@
 package api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
 
@@ -13,4 +15,7 @@ interface FlickrApi {
                 "&extras=url_s"
     )
     fun fetchPhotos() : Call<FlickrResponse>
+
+    @GET //causes retrofit to override the base url completely
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,11 +1,16 @@
 package com.bignerdranch.android.photogallery
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -25,11 +30,18 @@ private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
+    private lateinit var thumbnailDownloader:
+            ThumbnailDownloader<PhotoHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+        retainInstance = true
+        photoGalleryViewModel =
+            ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+
+        thumbnailDownloader = ThumbnailDownloader()
+        lifecycle.addObserver(thumbnailDownloader)
     }
 
     override fun onCreateView(
@@ -56,19 +68,32 @@ class PhotoGalleryFragment : Fragment() {
             })
     }
 
-    private class PhotoHolder(itemTextView: TextView) :
-        RecyclerView.ViewHolder(itemTextView){
-
-        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+    override fun onDestroy(){
+        super.onDestroy()
+        lifecycle.removeObserver(
+            thumbnailDownloader
+        )
     }
 
-    private class PhotoAdapter(private val galleryItems:
+    private class PhotoHolder(private val itemImageView: ImageView)
+        : RecyclerView.ViewHolder(itemImageView) {
+
+        val bindDrawable: (Drawable) -> Unit =
+            itemImageView::setImageDrawable
+    }
+
+    private inner class PhotoAdapter(private val galleryItems:
         List<GalleryItem>) : RecyclerView.Adapter<PhotoHolder>(){
 
         override fun onCreateViewHolder(parent: ViewGroup,
                                         viewType: Int): PhotoHolder {
-            val textView = TextView(parent.context)
-            return PhotoHolder(textView)
+            val view = layoutInflater.inflate(
+                R.layout.list_item_gallery,
+                parent,
+                false
+            ) as ImageView
+
+            return PhotoHolder(view)
         }
 
         override fun getItemCount() : Int = galleryItems.size
@@ -76,7 +101,17 @@ class PhotoGalleryFragment : Fragment() {
         override fun onBindViewHolder(holder: PhotoHolder,
                                       position: Int) {
             val galleryItem = galleryItems[position]
-            holder.bindTitle(galleryItem.title)
+
+            //provide blank ColorDrawable object if ContextCompat.getDrawable(...)
+            // returns null
+            val placeholder: Drawable = ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.bill_up_close
+            ) ?: ColorDrawable()
+            holder.bindDrawable(placeholder)
+
+
+            thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
         }
     }
 
